327c1d1982a78efdf121ab82f9c4310ed2d56422
tanekelly <tane.kelly15@gmail.com>
2025-03-30 23:30:29 +1300

    adjust player movement

--- Entities/Player/Data/player.gd
+++ Entities/Player/Data/player.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 520.0
+const JUMP_VELOCITY = -520.0
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 2
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
